fb50a319b013e4214eacf7be2e69a78fb93f5f6d
ChiaraMameli <mameli.chiara@libero.it>
2022-07-28 18:37:54 +0200

    created component SearchBar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <div>
-    <input type="text" v-model="query">
-    <button @click="getMovies">cerca</button>
-  </div>
+  <SearchBar @search-movie="getData"/>
 </template>
 
 <script>
 import axios from 'axios'
+import SearchBar from './components/SearchBar.vue';
 
 export default {
   name: 'App',
   components: {
+    SearchBar
   },
-  data(){
-    return{
-      query: '',
-    }
+ data(){
+      return {
+        query: ''}
   },
   methods:{
+    getData(query){
+      this.query = query
+
+      this.getMovies()
+    },
 
     getMovies(){
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=2acda1a8a6a5a953521ce22c6319420c&query=${this.query}`).then((res) => {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=2acda1a8a6a5a953521ce22c6319420c&query=${this.query}`).then((res) => {
       console.log(res.data.results)
     })}
   }
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input type="text" v-model="query">
+        <button @click="getItem">cerca</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data(){
+        return{
+            query: '',
+        }
+    },
+    methods:{
+        getItem(){
+            this.$emit('search-movie', this.query)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
