45878fdee849646d94a82039c14c315dd5d027cb
ChiaraMameli <mameli.chiara@libero.it>
2022-08-01 12:20:59 +0200

    added compontent and posters

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,22 @@
 <template>
 <div>
   <SearchBar @search-movie="getData"/>
-  <div id="sections">
-    <section v-if="searchedMovies.length > 0">
-      <h2>MOVIES:</h2>
-      <ul v-for="movie in searchedMovies" :key="movie.id">
-        <li>titolo: {{movie.title}}</li>
-        <li>titolo originale: {{movie.originalTitle}}</li>
-        <li>lingua: {{movie.originalLanguage}}</li>
-        <li>popolarità {{movie.popularity}}</li>
-      </ul>
-    </section>
-
-    <section v-if="searchedTvSeries.length > 0">
-      <h2>TV SERIES:</h2>
-      <ul v-for="serie in searchedTvSeries" :key="serie.id">
-        <li>titolo: {{serie.title}}</li>
-        <li>titolo originale: {{serie.originalTitle}}</li>
-        <li>lingua: {{serie.originalLanguage}}</li>
-        <li>popolarità; {{serie.popularity}}</li>
-      </ul>
-    </section>
-  </div>
+  <SearchedCard placeholder="Movies" :searchedItems="searchedMovies" v-if="searchedMovies.length > 0"/>
+  <SearchedCard placeholder="TV Series" :searchedItems="searchedTvSeries" v-if="searchedTvSeries.length > 0"/>
 </div>
 </template>
 
 <script>
 import axios from 'axios'
 import SearchBar from './components/SearchBar.vue';
+import SearchedCard from './components/SearchedCard.vue';
 
 export default {
   name: 'App',
   components: {
-    SearchBar
-  },
+    SearchBar,
+    SearchedCard
+},
  data(){
       return {
         query: '',
@@ -55,12 +38,13 @@ export default {
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=2acda1a8a6a5a953521ce22c6319420c&query=${this.query}`).then((res) => {
         const movies = res.data.results;
         movies.forEach(movie => {
+          const poster = `https://image.tmdb.org/t/p/w342${movie.poster_path}`
           const title = movie.title
           const originalTitle = movie.original_title
           const originalLanguage = movie.original_language
           const id = movie.id
-          const popularity = movie.popularity
-          movie = {title, originalTitle, originalLanguage, popularity, id}
+          const popularity = movie.vote_average
+          movie = {poster, title, originalTitle, originalLanguage, popularity, id}
           this.searchedMovies.push(movie)
         })})},
 
@@ -70,12 +54,13 @@ export default {
         const series = res.data.results;
         console.log(series)
         series.forEach(serie => {
+          const poster = `https://image.tmdb.org/t/p/w342${serie.poster_path}`
           const title = serie.name
           const originalTitle = serie.original_name
           const originalLanguage = serie.original_language
           const id = serie.id
-          const popularity = serie.popularity
-          serie = {title, originalTitle, originalLanguage, popularity, id}
+          const popularity = serie.vote_average
+          serie = {poster, title, originalTitle, originalLanguage, popularity, id}
           this.searchedTvSeries.push(serie)
       })})}
     
@@ -84,8 +69,4 @@ export default {
 </script>
 
 <style lang="scss">
-#sections{
-  display: flex;
-  justify-content:space-between;
-}
 </style> =
---
+++ src/components/SearchedCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h2>{{placeholder}}</h2>
+    <ul v-for="item in searchedItems" :key="item.id">
+        <li><img :src="item.poster" alt=""></li>
+        <li>titolo: {{item.title}}</li>
+        <li>titolo originale: {{item.originalTitle}}</li>
+        <li>lingua: {{item.originalLanguage}}</li>
+        <li>popolarità {{item.popularity}}</li>
+    </ul>
+
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'SearchedCard',
+    props:{
+        searchedItems: Array,
+        placeholder: String,
+    }
+}
+</script>
+
+<style>
+
+</style>
