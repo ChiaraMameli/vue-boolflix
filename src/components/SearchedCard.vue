3939d77dccfcac6c3d2124245431860d6ade9a18
ChiaraMameli <mameli.chiara@libero.it>
2022-08-02 17:52:23 +0200

    added flags

--- src/components/SearchedCard.vue
+++ src/components/SearchedCard.vue
@@ -3,9 +3,13 @@
     <h2>{{placeholder}}</h2>
     <ul v-for="item in searchedItems" :key="item.id">
         <li><img :src="item.poster" alt=""></li>
-        <li>titolo: {{item.title}}</li>
-        <li>titolo originale: {{item.originalTitle}}</li>
-        <li>lingua: {{item.originalLanguage}}</li>
+        <li>{{item.title}}</li>
+        <li>{{item.originalTitle}}</li>
+        <li>
+            <img v-if="flags.includes(item.originalLanguage)" :src="require(`../assets/img/${item.originalLanguage}.png`)" :alt="item.originalLanguage">
+            <span v-else>{{item.originalLanguage}}</span>
+        </li>
+            
         <li><span v-for="(star, i) in item.popularity" :key="i"><i class="fa-solid fa-star"></i></span></li>
     </ul>
 
@@ -19,6 +23,11 @@ export default {
         searchedItems: Array,
         placeholder: String,
     },
+    data(){
+        return{
+            flags: ['it', 'en']
+        } 
+    }
 }
 </script>
 
